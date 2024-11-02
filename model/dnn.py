439652218af2aba2aa60f3540f3c082dd5447c0e
anindyahepth <129802283+anindyahepth@users.noreply.github.com>
2024-11-01 20:41:45 -0400

    Create dnn.py

---
+++ model/dnn.py
@@ -0,0 +1,70 @@
+class DNN(nn.Module):
+  def __init__(self, input_dim, hidden_dim, num_class, depth):
+    super().__init__()
+
+    self.Lin1 = nn.Linear(input_dim, hidden_dim)
+    self.act1 = nn.Sigmoid()
+    self.layers = nn.ModuleList([])
+    for _ in range(depth):
+       self.layers.append(nn.ModuleList([
+          nn.Linear(hidden_dim, hidden_dim),
+          nn.Sigmoid()
+        ]))
+    self.Linf = nn.Linear(hidden_dim, hidden_dim)
+    self.actf = nn.Sigmoid()
+    self.out = nn.Linear(hidden_dim, num_class)
+    self.input_dim = input_dim
+
+
+  def forward(self, x):
+    size = x.size()
+    b = size[0]
+    x = rearrange(x, 'b c h w -> b (c h w)', b=b)
+    x = self.Lin1(x)
+    x = self.act1(x)
+    for Lin, act in self.layers:
+      x = Lin(x)
+      x = act(x)
+    x = self.Linf(x)
+    #y = x[:,99]
+    x = self.actf(x)
+    x = self.out(x)
+    return x
+
+
+
+class DNNBN(nn.Module):
+  def __init__(self, input_dim, hidden_dim, num_class, depth):
+    super().__init__()
+
+    self.Lin1 = nn.Linear(input_dim, hidden_dim)
+    self.norm1 = Batch_Norm(hidden_dim)
+    self.act1 = nn.Sigmoid()
+    self.layers = nn.ModuleList([])
+    for _ in range(depth):
+       self.layers.append(nn.ModuleList([
+          nn.Linear(hidden_dim, hidden_dim),
+          Batch_Norm(hidden_dim),
+          nn.Sigmoid()
+        ]))
+    self.Linf = nn.Linear(hidden_dim, hidden_dim)
+    self.normf = Batch_Norm(hidden_dim)
+    self.actf = nn.Sigmoid()
+    self.out = nn.Linear(hidden_dim, num_class)
+
+  def forward(self, x):
+    size = x.size()
+    b = size[0]
+    x = rearrange(x, 'b c h w -> b (c h w)', b=b)
+    x = self.norm1(self.Lin1(x))
+    x = self.act1(x)
+    for Lin, norm, act in self.layers:
+      x = Lin(x)
+      x = norm(x)
+      x = act(x)
+    x = self.normf(self.Linf(x))
+    # w = x[:,0]
+    x = self.actf(x)
+    x = self.out(x)
+    return x
+
