cb01fdf9b14190973b00b6ce9ccab6ed3ca51f09
anindyahepth <129802283+anindyahepth@users.noreply.github.com>
2024-11-03 12:15:30 -0500

    Update test.py

--- model/test.py
+++ model/test.py
@@ -93,7 +93,7 @@ class Transformer(nn.Module):
 
         return self.norm(x)
 
-class ViT(nn.Module):
+class ViTBNFFN(nn.Module):
     def __init__(self, *, image_size, patch_size, num_classes, dim, depth, heads, mlp_dim, pool = 'cls', channels = 3, dim_head = 64, dropout = 0., emb_dropout = 0.):
         super().__init__()
         image_height, image_width = pair(image_size)
